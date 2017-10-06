3b1be230880251045b404ae0fb11b3153c44e4da
Eugene Sadovoi <evgeni@eniks.com>
2017-10-05 22:44:10 -0400

    Added new project

---
+++ tests/Components/AdvancedLogger.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Practices.Unity.ServiceLocation.Tests.Components
+{
+    public class AdvancedLogger : ILogger
+    {
+        public void Log(string msg)
+        {
+        }
+    }
+}
---
+++ tests/Components/SimpleLogger.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Practices.Unity.ServiceLocation.Tests.Components
+{
+    public class SimpleLogger : ILogger
+    {
+        public void Log(string msg)
+        {
+        }
+    }
+}
